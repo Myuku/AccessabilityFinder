3ba1fc85f4fe98d64ce3ceb0a16cf37d96507762
Myuku <Junelee2002jh@gmail.com>
2023-05-13 11:37:20 -0700

    Imported navbar

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include "includes/header.html" ?>
+
+<body>
+    <?php include "includes/navbar.html" ?>
+
+    <section class="home">
+        <h1>hi</h1>
+    </section>
+    
+
+</body>
+
+<include src="./footer.html"></include>
+
+</html>
