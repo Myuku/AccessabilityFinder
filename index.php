1a49dcce3d9a9e6b9b235755a07a1a29bda25f2b
Myuku <Junelee2002jh@gmail.com>
2023-05-13 12:40:33 -0700

    Did stuff

--- index.php
+++ index.php
@@ -5,9 +5,33 @@
 <body>
     <?php include "includes/navbar.html" ?>
 
-    <section class="home">
-        <h1>hi</h1>
+    <section class="main">
+        <div class="comments">
+            awdawdawe
+
+
+
+
+
+
+
+
+
+        </div>
+        <div class="map">
+                <iframe
+                    height="100%"
+                    width="100%"
+                    style="border:0"
+                    loading="lazy"
+                    allowfullscreen
+                    referrerpolicy="no-referrer-when-downgrade"
+                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCBq7-eWrP0E0JORqLGv14We7saZT1VciQ
+                    &q=Space+Needle,Simon Fraser University">
+                </iframe>
+            </div>
     </section>
+
     
 
 </body>
