74e069f9bee400f26f9ac522ef3fdef2e2e3f8e6
jennaolee <105249401+jennaolee@users.noreply.github.com>
2023-05-13 15:10:32 -0700

    store and display existing markers after visiting a location

--- index.php
+++ index.php
@@ -42,26 +42,8 @@
                 }
             </script>
 
-
-
-
-
-
-
-
         </div>
         <div class="map">
-            <!-- <iframe
-                height="100%"
-                width="100%"
-                style="border:0"
-                loading="lazy"
-                allowfullscreen
-                referrerpolicy="no-referrer-when-downgrade"
-                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCBq7-eWrP0E0JORqLGv14We7saZT1VciQ
-                &q=Space+Needle,Simon Fraser University">
-            </iframe> -->
-
 
             <div style="display: none">
                 <input
@@ -76,6 +58,12 @@
                 <span id="place-name" class="title"></span><br />
                 <strong>Place ID:</strong> <span id="place-id"></span><br />
                 <span id="place-address"></span>
+                <strong>Rating:</strong> <span id="rating"></span> <br />
+                <span id="place-rating" ></span>
+                <strong>Geometry Location:</strong> <span id="geometry-location"></span> <br />
+                <span id="place-geometry-location" ></span>
+                <!-- <strong>Wheelchair Accessible Entrance:</strong> <span id="wheelchair-accessible-entrance"></span> <br />
+                <span id="place-wheelchair-accessible-entrance" ></span> -->
             </div>
 
             <script
@@ -84,61 +72,86 @@
             ></script>
 
             <script>
+                
+                let markers = [];
+
+                function setMarkers(map) {
+                    for (let i = 0; i < markers.length; i++) {
+                        let marker = markers[i];
+                        let newMarker = new google.maps.Marker({
+                            position: {lat: marker.geometry.location.lat(), lng: marker.geometry.location.lng()},
+                            map,
+                            title: marker.name
+
+                        })
+                        console.log(newMarker);
+                    }
+                }
 
                 function initMap() {
-                const map = new google.maps.Map(document.getElementById("map"), {
-                    center: { lat: 49.2488, lng: -122.9805 },
-                    zoom: 13,
-                });
-                const input = document.getElementById("pac-input");
-                // Specify just the place data fields that you need.
-                const autocomplete = new google.maps.places.Autocomplete(input, {
-                    fields: ["place_id", "geometry", "formatted_address", "name"],
-                });
 
-                autocomplete.bindTo("bounds", map);
-                map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-                const infowindow = new google.maps.InfoWindow();
-                const infowindowContent = document.getElementById("infowindow-content");
-
-                infowindow.setContent(infowindowContent);
-
-                const marker = new google.maps.Marker({ map: map });
-
-                marker.addListener("click", () => {
-                    infowindow.open(map, marker);
-                });
-                autocomplete.addListener("place_changed", () => {
-                    infowindow.close();
-
-                    const place = autocomplete.getPlace();
-
-                    if (!place.geometry || !place.geometry.location) {
-                    return;
-                    }
-
-                    if (place.geometry.viewport) {
-                    map.fitBounds(place.geometry.viewport);
-                    } else {
-                    map.setCenter(place.geometry.location);
-                    map.setZoom(17);
-                    }
-
-                    // Set the position of the marker using the place ID and location.
-                    // @ts-ignore This should be in @typings/googlemaps.
-                    marker.setPlace({
-                    placeId: place.place_id,
-                    location: place.geometry.location,
+                    const map = new google.maps.Map(document.getElementById("map"), {
+                        center: { lat: 49.2488, lng: -122.9805 },
+                        zoom: 13,
                     });
-                    marker.setVisible(true);
-                    infowindowContent.children.namedItem("place-name").textContent = place.name;
-                    infowindowContent.children.namedItem("place-id").textContent =
-                    place.place_id;
-                    infowindowContent.children.namedItem("place-address").textContent =
-                    place.formatted_address;
-                    infowindow.open(map, marker);
-                });
+
+                    const input = document.getElementById("pac-input");
+                    // Specify just the place data fields that you need.
+                    const autocomplete = new google.maps.places.Autocomplete(input, {
+                        fields: ["place_id", "geometry", "formatted_address", "name", "rating", "geometry/location"],
+                    });
+
+                    autocomplete.bindTo("bounds", map);
+                    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+                    const infowindow = new google.maps.InfoWindow();
+                    const infowindowContent = document.getElementById("infowindow-content");
+
+                    infowindow.setContent(infowindowContent);
+
+                    const marker = new google.maps.Marker({ map: map });
+                    
+                    marker.addListener("click", () => {
+                        infowindow.open(map, marker);
+                    });
+                    autocomplete.addListener("place_changed", () => {
+                        infowindow.close();
+
+                        const place = autocomplete.getPlace();
+                        // ADD MARKER TO MAP 
+                        markers.push(place)
+                        console.log(markers.length)
+                        console.log(markers)
+                        setMarkers(map);
+
+                        if (!place.geometry || !place.geometry.location) {
+                        return;
+                        }
+
+                        if (place.geometry.viewport) {
+                        map.fitBounds(place.geometry.viewport);
+                        } else {
+                        map.setCenter(place.geometry.location);
+                        map.setZoom(17);
+                        }
+
+                        // Set the position of the marker using the place ID and location.
+                        // @ts-ignore This should be in @typings/googlemaps.
+                        marker.setPlace({
+                        placeId: place.place_id,
+                        location: place.geometry.location,
+                        });
+                        marker.setVisible(true);
+                        infowindowContent.children.namedItem("place-name").textContent = place.name;
+                        infowindowContent.children.namedItem("place-id").textContent =
+                        place.place_id;
+                        infowindowContent.children.namedItem("place-address").textContent =
+                        place.formatted_address;
+                        infowindowContent.children.namedItem("place-rating").textContent = place.rating;
+                        infowindowContent.children.namedItem("place-geometry-location").textContent = place.geometry.location;
+                        // infowindowContent.children.namedItem("place-wheelchair-accessible-entrance").textContent = place.wheelchair_accessible_entrance;
+                        infowindow.open(map, marker);
+                    });
                 }
 
                 window.initMap = initMap;
